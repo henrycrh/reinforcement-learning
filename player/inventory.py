4e5399ed91b0580ca0cc41ac1ed53c9071138430
ThatOneGoat <mcduan01@gmail.com>
2022-12-18 21:50:24 -0800

    reminder

--- player/inventory.py
+++ player/inventory.py
@@ -1,5 +1,6 @@
 class Inventory:
     def __init__(self, items):
+        # TODO: items will store an Items object with properties like quantity in place of a singular count
         self.items = [0 for x in range(len(items))]
 
     def add_good(self, item, count):
