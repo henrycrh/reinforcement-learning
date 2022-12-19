f25a4bd92d9aa41aeaac3f89c73247b6e57bc986
ThatOneGoat <mcduan01@gmail.com>
2022-12-18 22:12:51 -0800

    gets mutable copy of item when building ivnentory

--- player/inventory.py
+++ player/inventory.py
@@ -3,7 +3,7 @@ from ..item import Good, Investment
 class Inventory:
     def __init__(self, items):
         # TODO: items will store an Items object with properties like quantity in place of a singular count
-        self.items = [Good() if x.type == "good" else Investment() for x in range(len(items))]
+        self.items = [x.get_mutable() for x in range(len(items))]
 
     def add_good(self, item, count):
         self.items[item] += count
